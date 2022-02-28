f8f9ed818f43720cc28287864d67609f4e22338d
Manuel Escobar <j.manuel_e@hotmail.com>
2022-02-27 22:51:46 -0600

    added orgvsm assets

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,82 +1,38 @@
-import {Grid,useMediaQuery} from '@mui/material';
-import logo from '../images/footer_logoWithText_white.svg';
-import fsIcon1 from '../images/fsIcon1.svg';
-import fsIcon2 from '../images/fsIcon2.svg';
-import { useTheme } from '@mui/material/styles';
+import Button from "@mui/material/Button";
+import logo from "../images/newLogo.png";
+import Grid from "@mui/material/Grid";
 
-function Footer(){
-    const theme = useTheme();
-    const smallScreen = useMediaQuery(theme.breakpoints.down('md'));
-    return(
-        <Grid container className='footer mb-0' px={smallScreen?1:5}>
-           <Grid container>
-               <Grid item xs={12} md={4} px={smallScreen?1:5} >
-                 <img src={logo} alt='logo' className='footerLogo w-100'/>
-               </Grid>
-               <Grid item xs={12} md={8}>
-                    <Grid container p={smallScreen?1:5} >
-                        <Grid item xs={6} md={3}>
-                            <p className='footerSmText'>RESTAURANT</p>
-                            <a className='footerItem'>Home</a><br/>
-                            <a className='footerItem'>About</a><br/>
-                            <a className='footerItem'>Become a Member</a>
-                        </Grid>
-                        <Grid item xs={6} md={3}>
-                            <p className='footerSmText'>HELP</p>
-                            <a className='footerItem'>How It Works</a><br/>
-                            <a className='footerItem'>FAQs</a><br/>
-                            <a className='footerItem'>Contact</a>
-                        </Grid>
-                        <Grid item xs={6} md={3}>
-                            <p className='footerSmText'>Contact</p>
-                            <a className='footerItem'>Member Sign In</a><br/>
-                            <a className='footerItem'>Reservations</a><br/>
-                        </Grid>
-                        <Grid item xs={6} md={3}>
-                            <p className='footerSmText'>LEGAL</p>
-                            <a className='footerItem'>Terms of Use</a><br/>
-                            <a className='footerItem'>Privacy Policy</a><br/>
-                        </Grid>
-                    </Grid>
-               </Grid>
-           </Grid>
-           <Grid container px={5} className='hrFooter mt-5'></Grid>
-           <Grid container px={5} className='hrFooter mt-1'></Grid>
-           <Grid container  mt={2} justifyContent={'space-between'}>
-                <Grid item md={6}>
-                  <Grid container>
-                    <Grid item xs={6} md={3} className=''>
-                        <div style={{display:'flex',alignItems:'center'}}>
-                            <img src={fsIcon1} alt='fsIcon1' className='socialIcon' />
-                            <p className='socialIconText my-0'>Flyfish Club</p>
-                         </div>
-                    </Grid>
-                    <Grid item xs={6} md={3}>
-                        <div style={{display:'flex',alignItems:'center'}}>
-                            <img src={fsIcon2} alt='fsIcon1' className='socialIcon' />
-                            <p className='socialIconText my-0'>Flyfish Club</p>
-                         </div>
-                    </Grid>
-                    <Grid item xs={6} md={3}>
-                        <div style={{display:'flex',alignItems:'center'}}>
-                            <img src={fsIcon1} alt='fsIcon1' className='socialIcon' />
-                            <p className='socialIconText my-0'>Flyfish Club</p>
-                         </div>
-                    </Grid>
-                    <Grid item xs={6} md={3}>
-                        <div style={{display:'flex',alignItems:'center'}}>
-                            <img src={fsIcon1} alt='fsIcon1' className='socialIcon' />
-                            <p className='socialIconText my-0'>Flyfish Club</p>
-                         </div>
-                    </Grid>
-                  </Grid>
-                </Grid>
-                <Grid item md={6}>
-                    <p className='copyri8Text'>Copyright ©2021-2022 VCR Group. All rights reserved.</p>
-                    <p className='copyri8Text'>For all press and media inquiries, email <a className='footerEmail'>press@vcrgroup.com</a></p>
-                </Grid>
-           </Grid>
+function Footer() {
+  return (
+    <Grid container >
+      <Grid
+        container
+        px={4}
+        mt={5}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        style={{ background: "black", height: "10px" }}
+      >
+        <Grid item xs={8}>
+          <div style={{ width: "50px", height: "10%" }}>
+            <img
+              src={logo}
+              style={{ transform: " scale3d(1.5, 1.5, 1.5)", width: "100%" }}
+              className="w-100"
+            />
+          </div>
         </Grid>
-    )
+        <Grid item xs={4}>
+          <Grid container justifyContent={"space-between"} display={"flex"}>
+            <i class="fa-brands fa-instagram sicons"></i>
+            <i class="fa-brands fa-twitter sicons"></i>
+            <i class="fa-brands fa-facebook sicons"></i>
+            <i class="fa-brands fa-discord sicons"></i>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,92 +1,79 @@
-import React from 'react';
-import { Navbar, Nav, Container,Button } from 'react-bootstrap';
-import logoWithText from '../images/logoWithText.svg';
-import logo from '../images/logo.svg';
-import {Grid,useMediaQuery} from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-
+import Button from '@mui/material/Button';
+import logo from "../images/newLogo.png";
 import Swal from 'sweetalert2'
 import Web3 from 'web3';
 require('dotenv').config()
 
-function Header(){
-  const theme = useTheme();
-  const smallScreen = useMediaQuery(theme.breakpoints.down('md'));
 
-  async function getCurrentAccount() {
-    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-    return accounts[0];
-  }
+function Header() {
 
-  async function handelConnect() {
-      try {
-          if (typeof window.ethereum === 'undefined') {
-              Swal.fire({
-                  icon: 'info',
-                  title: 'Metamask is not installed',
-                  footer: '<a target="_blank" href="https://metamask.io/">Install Metamask 🦊</a>'
-              })
-          } else {
-              const currentAccount = await getCurrentAccount()
-              if (typeof currentAccount === 'undefined') { 
-                  try {
-                      window.web3 = new Web3(window.ethereum);
-                      window.ethereum.enable();
-                      const localContract = require("../contracts/" + process.env.REACT_APP_CONTRACT_FILE)
-                      const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS
-                      window.contract = await new window.web3.eth.Contract(localContract.abi, contractAddress);
-                  }
-                  catch (err) {
-                      console.log(err)
-                  }
-              } else {
+    async function getCurrentAccount() {
+        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+        return accounts[0];
+      }
+    
+      async function handleConnect() {
+          try {
+              if (typeof window.ethereum === 'undefined') {
                   Swal.fire({
-                      icon: 'success',
-                      title: 'You are already connected using Metamask',
-                      text: 'Connected address: ' + currentAccount
+                      icon: 'info',
+                      title: 'Metamask is not installed',
+                      footer: '<a target="_blank" href="https://metamask.io/">Install Metamask 🦊</a>'
                   })
+              } else {
+                  const currentAccount = await getCurrentAccount()
+                  if (typeof currentAccount === 'undefined') { 
+                      try {
+                          window.web3 = new Web3(window.ethereum);
+                          window.ethereum.enable();
+                          const localContract = require("../contracts/" + process.env.REACT_APP_CONTRACT_FILE)
+                          const contractAddress = process.env.REACT_APP_CONTRACT_ADDRESS
+                          window.contract = await new window.web3.eth.Contract(localContract.abi, contractAddress);
+                      }
+                      catch (err) {
+                          console.log(err)
+                      }
+                  } else {
+                      Swal.fire({
+                          icon: 'success',
+                          title: 'You are already connected using Metamask',
+                          text: 'Connected address: ' + currentAccount
+                      })
+                  }
               }
           }
+          catch(err) {
+              console.log(err)
+          }
       }
-      catch(err) {
-          console.log(err)
-      }
-  }
-  return (
-  <>
-  {
-    smallScreen?
-     <>
-      <div className='navBottomBorder'></div>
-      <Navbar  variant="light"  className='NavBAR px-3'>
-        <Grid container  justifyContent={'space-between'} alignItems={'center'} >
-        <Navbar.Brand className='headerLogo' href="#home"><img src={logo} alt='logo' className='w-100' /></Navbar.Brand>
-        <Nav className="me-auto navItem">
-          <Nav.Link href="#home">About</Nav.Link>
-          <Nav.Link href="#features">How it works</Nav.Link>
-          <Nav.Link href="#pricing">Become a member</Nav.Link>
-          <Button className='headerBtnSM pr-0'>Sign In</Button>
-        </Nav>
-        </Grid>
-      </Navbar>
-    </>
-    :
-    <>
-      <div className='navBottomBorder'></div>
-      <Navbar  variant="light" className='NavBAR'>
-        <Grid container px={3} justifyContent={'space-between'} alignItems={'center'} >
-        <Navbar.Brand className='headerLogo' href="#home"><img src={logoWithText} alt='logo' className='w-100' /></Navbar.Brand>
-        <Nav className="me-auto navItem">
-          <Nav.Link href="#home">About</Nav.Link>
-          <Nav.Link href="#features">How it works</Nav.Link>
-          <Nav.Link href="#pricing">Become a member</Nav.Link>
-          <Button onClick={handelConnect} className='headerBtn'>Connect Wallet</Button>
-        </Nav>
-        </Grid>
-      </Navbar>
-    </>
-  }
-  </>
-  );
+      return (
+        <div
+          className="px-4 py-3"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            height: "70px",
+          }}
+        >
+          <div style={{ width: "150px" }}>
+            <img src={logo} style={{ transform: "scale(1.5)" }} className="w-100" />
+          </div>
+          <Button
+            onClick={handleConnect}
+            variant="text"
+            style={{
+              outline: "none",
+              border: "3px solid #fff",
+              color: "#fff",
+              fontWeight: "bold",
+    
+              borderRadius: "50px",
+            }}
+          >
+            Connect Wallet
+          </Button>
+        </div>
+      );
 }
 export default Header;
